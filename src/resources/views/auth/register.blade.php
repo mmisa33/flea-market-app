8e2b5d7b35b7de647ef0556d3e15eb01c0d7474f
misa <mmichiura33@gmail.com>
2025-03-21 21:38:11 +0900

    会員登録ページの基本ビューを作成

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -9,5 +9,65 @@
     {{--  ページタイトル  --}}
     <h2 class="register-form__heading content__heading">会員登録</h2>
 
+    {{--  ユーザー登録フォーム  --}}
+    <div class="register-form__inner">
+        <form class="register-form__form" action="/register" method="post">
+            @csrf
+            <div class="register-form__group">
+                <label class="register-form__label" for="name">ユーザー名</label>
+                <input class="register-form__input" type="text" name="name" id="name">
+                {{--  エラーメッセージ  --}}
+                <p class="register-form__error-message">
+                @error('name')
+                {{ $message }}
+                @enderror
+                </p>
+            </div>
+
+            {{--  メールアドレス入力  --}}
+            <div class="register-form__group">
+                <label class="register-form__label" for="email">メールアドレス</label>
+                <input class="register-form__input" type="mail" name="email" id="email">
+                {{--  エラーメッセージ  --}}
+                <p class="register-form__error-message">
+                @error('email')
+                {{ $message }}
+                @enderror
+                </p>
+            </div>
+
+            {{--  パスワード入力  --}}
+            <div class="register-form__group">
+                <label class="register-form__label" for="password">パスワード</label>
+                <input class="register-form__input" type="password" name="password" id="password">
+                {{--  エラーメッセージ  --}}
+                <p class="register-form__error-message">
+                @error('password')
+                {{ $message }}
+                @enderror
+                </p>
+            </div>
+
+            {{--  確認用パスワード入力  --}}
+            <div class="register-form__group">
+                <label class="register-form__label" for="confirm_password">確認用パスワード</label>
+                <input class="register-form__input" type="password" name="confirm_password" id="confirm_password">
+                {{--  エラーメッセージ  --}}
+                <p class="register-form__error-message">
+                @error('confirm-password')
+                {{ $message }}
+                @enderror
+                </p>
+            </div>
+
+            <div class="confirm-form__btn-inner">
+                {{--  登録ボタン  --}}
+                <input class="register-form__btn" type="submit" value="登録する">
+
+                {{--  ログインページへ移行  --}}
+                <a class="register-form__link" href="/login">ログインはこちら</a>
+            </div>
+        </form>
+    </div>
 </div>
 @endsection
