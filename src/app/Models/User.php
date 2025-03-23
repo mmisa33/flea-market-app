3a3c55fa9c64447a3510bc0d7fc715a5efd1d1d3
misa <mmichiura33@gmail.com>
2025-03-23 16:45:06 +0900

    コメントのテーブルとモデルを作成

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -61,4 +61,9 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Item::class, 'item_likes');
     }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
