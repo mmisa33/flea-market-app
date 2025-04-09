63448ed86bf67bf46df95ce3392ed3d434d350bf
misa <mmichiura33@gmail.com>
2025-04-09 19:57:45 +0900

    ルーティングのコードを整理

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,44 +1,42 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PurchaseController;
+use Illuminate\Support\Facades\Route;
 
 // ログアウト処理
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-// トップページ
+// トップページ表示・検索機能
 Route::get('/', [ItemController::class, 'index'])->name('home');
 Route::get('/search', [ItemController::class, 'index'])->name('items.search');
 
-// 商品詳細ページ
+// 商品詳細・コメント・いいね機能
 Route::get('/item/{item_id}', [ItemController::class, 'show'])->name('item.show');
 Route::post('/item/{item_id}/comment', [ItemController::class, 'comment'])->name('item.comment');
 Route::post('/item/{item}/like', [ItemController::class, 'like'])->name('item.like');
 
-// 商品購入ページ
+// ログインユーザー専用ページ
 Route::middleware(['auth'])->group(function () {
+
+    // 商品購入
     Route::get('/purchase/{item}', [PurchaseController::class, 'show'])->name('item.purchase');
     Route::post('/purchase/{item}', [PurchaseController::class, 'submit'])->name('item.purchase.submit');
-});
 
-// 住所変更ページ
-Route::middleware(['auth'])->group(function () {
+    // 配送先住所の変更
     Route::get('/purchase/address/{item}', [PurchaseController::class, 'showAddressEdit'])->name('purchase.address.edit');
     Route::patch('/purchase/address/{item}', [PurchaseController::class, 'updateAddress'])->name('purchase.address.update');
-});
 
-// 出品ページ
-Route::middleware(['auth'])->group(function () {
+    // 商品出品
     Route::get('/sell', [ItemController::class, 'create'])->name('item.create');
     Route::post('/sell', [ItemController::class, 'store'])->name('item.store');
-});
 
-// プロフィール関連
-Route::middleware(['auth'])->prefix('mypage')->name('profile.')->group(function () {
-    Route::get('/', [ProfileController::class, 'show'])->name('show');
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('update');
+    // マイページ（プロフィール）
+    Route::prefix('mypage')->name('profile.')->group(function () {
+        Route::get('/', [ProfileController::class, 'show'])->name('show');
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('update');
+    });
 });
