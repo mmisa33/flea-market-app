6a5bc6d5013c8d482ba724ac182dae9b9f048c13
misa <mmichiura33@gmail.com>
2025-04-14 11:58:35 +0900

    コメントおよび商品説明のテーブルとフォームリクエストを修正

--- src/database/migrations/2025_03_23_163402_create_comments_table.php
+++ src/database/migrations/2025_03_23_163402_create_comments_table.php
@@ -17,7 +17,7 @@ class CreateCommentsTable extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('item_id')->constrained()->cascadeOnDelete();
-            $table->string('content', 255);
+            $table->text('content');
             $table->timestamps();
         });
     }
